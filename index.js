70a5f657ec7b66458eadfea65a23225ca9815f92
Yomaira Escano <yjescano@gmail.com>
2019-02-11 03:46:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,59 +1,82 @@
-let store = {drivers: [], passengers: [], trips: []}
-let driverId = 0
-let passengerId = 0
-let tripId = 0
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
 
 class Driver {
-  constructor(name){
-    this.id = ++driverId
-    this.name = name
-    store.drivers.push(this)
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
   }
-  trips(){
-    return store.trips.filter(trip => {
-      return trip.driverId === this.id
-    })
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.driverId === this.id;
+    }.bind(this));
   }
-  passengers(){
-    return store.passengers
+
+  passengers() {
+    return store.passengers.filter(function(passenger) {
+      return passenger.driverId === this.driverId;
+    }.bind(this));
   }
 }
+
+let passengerId = 0;
 
 class Passenger {
-  constructor(name){
-    this.id = ++passengerId
-    this.name = name
-    store.passengers.push(this)
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
   }
-  trips(){
-    return store.trips.filter(trip => {
-      return trip.passengerId === this.id
-    })
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.passengerId === this.id;
+    }.bind(this));
   }
-  drivers(){
-    return store.drivers
+
+  drivers() {
+    return store.drivers.filter(function(driver) {
+      return driver.passengerId === this.passengerId;
+    }.bind(this));
   }
 }
 
+let tripId = 0;
+
 class Trip {
-  constructor(driver, passenger){
-    this.id = ++tripId
-    if(driver){
-      this.driverId = driver.id
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    if (driver) {
+      this.driverId = driver.id;
     }
-    if(passenger){
-      this.passengerId = passenger.id
+    if (passenger) {
+      this.passengerId = passenger.id;
     }
-    store.trips.push(this)
+    store.trips.push(this);
   }
-  driver(){
-    return store.drivers.find(function(driver){
-      return driver.id
-    })
+
+  setDriver(driver) {
+    this.driverId = driver.id;
   }
-  passenger(){
-    return store.passengers.find(function(passenger){
-      return passenger.id
-    })
+
+  setPassenger(passenger) {
+    this.passengerId = passenger.id;
+  }
+
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId;
+    }.bind(this));
+  }
+
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId;
+    }.bind(this));
   }
 }
