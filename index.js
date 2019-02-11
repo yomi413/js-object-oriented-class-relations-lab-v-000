03e46fca7babbcc1eabeef5782f0a26353e9247e
Yomaira Escano <yjescano@gmail.com>
2019-02-11 03:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
+
 class Driver {
   constructor(name) {
+    this.id = ++driverId;
     this.name = name;
+
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.driverId === this.id;
+    }.bind(this));
+  }
+
+  passengers() {
+    return store.passengers.filter(function(passenger) {
+      return passenger.driverId === this.driverId;
+    }.bind(this));
+  }
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(function(trip) {
+      return trip.passengerId === this.id;
+    }.bind(this));
+  }
+
+  drivers() {
+    return store.drivers.filter(function(driver) {
+      return driver.passengerId === this.passengerId;
+    }.bind(this));
+  }
+}
+
+let tripId = 0;
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    if (driver) {
+      this.driverId = driver.id;
+    }
+    if (passenger) {
+      this.passengerId = passenger.id;
+    }
+    store.trips.push(this);
+  }
+
+  setDriver(driver) {
+    this.driverId = driver.id;
+  }
+
+  setPassenger(passenger) {
+    this.passengerId = passenger.id;
+  }
+
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId;
+    }.bind(this));
+  }
+
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId;
+    }.bind(this));
   }
 }
 
